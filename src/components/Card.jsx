15d331aaa052edd2ba0d4f427c3156cf8c6b924c
Miguel Calixto <miguel.calixto98@hotmail.com>
2022-12-14 19:48:35 -0600

    Fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ export const Card = ({_id,title,image,description}) => {
     <Link to={'/detail-movie'}  state={_id} className="link text-black">
     <div class="col">
     <div class="card h-100" >
-      <img src={image} class="card-img-top" style={{maxHeight: '10.5rem'}} alt={title} />
+      <img src={image} class="card-img-top" style={{maxHeight: '10.3rem'}} alt={title} />
       <div class="card-body">
         <h5 class="card-title">{title}</h5>
         <p class="card-text">
