06af5184d6df3c01b07736ad0b8a181bdc0088d1
Miguel Calixto <miguel.calixto98@hotmail.com>
2022-12-14 19:31:01 -0600

    Grid image link to DetailMovie

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { Card } from "../components/Card";
 import { GET_MOVIES } from "../graphql/Queries";
 import { useLazyQuery } from "@apollo/client";
-
+import './Home.css'
 export const Home = () => {
   useEffect(() => {
     getMovies();
@@ -11,12 +11,13 @@ export const Home = () => {
   const [getMovies, { data, error }] = useLazyQuery(GET_MOVIES);
 
   return (
-    <div className="container">
-      <div class="row row-cols-1 row-cols-md-4 g-4">
+    <div className="container mt-4">
+      <div className="row row-cols-2 row-cols-md-4 g-4">
         {data &&
           data.getMovies.map(({ _id, title, description, image }) => (
             <Card
               key={_id}
+              _id={_id}
               title={title}
               description={description}
               image={image}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
-
+import { DetailMovie } from "../pages/DetailMovie";
 const MainRoutes = () => {
   return (
     <Routes>
       <Route index element={<Login />}></Route>
       <Route path="/home" element={<Home />}></Route>
+      <Route path="/detail-movie" element={<DetailMovie />}></Route>
     </Routes>
   );
 };
